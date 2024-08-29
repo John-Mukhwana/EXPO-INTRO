12492ecd6e230cf3355e1efeef98a8988dd13a38
John Mukhwana <Johnbradill67@gmail.com>
2024-08-29 21:53:17 +0300

    aded fonts

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      
+      className="bg-blue-900 flex-1 justify-center items-center"
+    >
+      <Text className="text-Green-950 bg">Edit app/index bato majoni fdbg.</Text>
+    
+    </View>
+  );
+}
