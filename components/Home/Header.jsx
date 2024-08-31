e11fec11c576542529f1a1da374de0161692350c
John Mukhwana <Johnbradill67@gmail.com>
2024-09-01 00:22:46 +0300

    done creating header component for home screen.

--- components/Home/Header.jsx
+++ components/Home/Header.jsx
@@ -1,21 +1,21 @@
 
 
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { useUser } from '@clerk/clerk-react'
 
 export default function Header() {
     const {user} = useUser()
   return (
-    <View>
+    <View className="flex flex-row justify-between items-center ">
          
          <View>
             <Text className="font-[outfit] text-[18px]">Welcome</Text>
             <Text className="font-[outfit-medium] text-[25px]">{user?.fullName}</Text>
          </View>
 
-         
-
+        <Image source ={{uri:user?.imageUrl}} className="w-[40px] h-[40px] rounded-3xl"/>
+        
 
     </View>
   )
