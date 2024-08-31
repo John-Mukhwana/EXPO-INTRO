27143dc3ed90ee0e1e715f655315cfdf9efc560a
John Mukhwana <Johnbradill67@gmail.com>
2024-08-31 23:36:09 +0300

    Modified the tabs by adding icons from expo icons.fyi

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -2,14 +2,47 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import {Tabs} from 'expo-router'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Colors from './../../constants/Colors'
+
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name='home'/>
-      <Tabs.Screen name='favorite'/>
-      <Tabs.Screen name='inbox'/>
-      <Tabs.Screen name='profile'/>
+    <Tabs   
+       screenOptions={{
+          tabBarActiveTintColor:Colors.PRIMARY
+       }}
+     >
+
+
+      <Tabs.Screen name='home'
+         options={{
+            title: 'Home',
+            headerShown: false,
+            tabBarIcon:({color})=><Ionicons name="home" size={24} color={color} />
+         }}
+       />
+      <Tabs.Screen name='favorite'
+          options={{
+            title: 'Favourite',
+            headerShown: false,
+            tabBarIcon:({color})=><Ionicons name="heart" size={24} color={color} />
+         }}
+      />
+      <Tabs.Screen name='inbox'
+         options={{
+          title: 'Inbox',
+          headerShown: false,
+          tabBarIcon:({color})=><Ionicons name="chatbubble" size={24} color={color} />
+       }}
+      />
+      <Tabs.Screen name='profile'
+         options={{
+          title: 'Profile',
+          headerShown: false,
+          tabBarIcon:({color})=><Ionicons name="people-circle" size={24} color={color} />
+       }}
+      />
     </Tabs>
   )
 }
