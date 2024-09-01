143338b47e5ffc6860eae66448c08d18356d63c5
John Mukhwana <Johnbradill67@gmail.com>
2024-09-01 16:42:25 +0300

    Fetched data from firbase uign async and await and useEffect

--- components/Home/Slider.jsx
+++ components/Home/Slider.jsx
@@ -1,9 +1,26 @@
 
 
 import { View, Text } from 'react-native'
-import React from 'react'
+import React,{useEffect} from 'react'
+import {db} from './../../config/FirebaseConfig'
+import { collection, getDocs } from "firebase/firestore";
+
 
 export default function Slider() {
+
+    useEffect( ()=>{
+      GetSliders();
+    },[])
+
+    const GetSliders=async()=>{
+         const snapshot = await getDocs(collection(db,'Sliders'));
+         snapshot.forEach((doc)=>{
+              console.log(doc.data());
+         })
+
+    }
+
+
   return (
     <View>
       <Text>Slider</Text>
