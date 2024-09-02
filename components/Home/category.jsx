7701543ba6e5f2013367f29b4fb9e528ed61839e
John Mukhwana <Johnbradill67@gmail.com>
2024-09-02 11:55:02 +0300

    created a function in category

---
+++ components/Home/category.jsx
@@ -0,0 +1,11 @@
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function category() {
+  return (
+    <View>
+      <Text>category</Text>
+    </View>
+  )
+}
