278ed055d9b9d6e2760513e95963f6dad4e89de4
John Mukhwana <Johnbradill67@gmail.com>
2024-09-02 11:57:06 +0300

    created a PetListByCategory file with function in it

---
+++ components/Home/PetListByCategory.jsx
@@ -0,0 +1,11 @@
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function PetListByCategory() {
+  return (
+    <View>
+      <Text>PetListByCategory</Text>
+    </View>
+  )
+}
