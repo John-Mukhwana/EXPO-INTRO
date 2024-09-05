32dbaef867c2dd57e397755bb71ebf15b76d6e6a
John Mukhwana <Johnbradill67@gmail.com>
2024-09-05 17:55:35 +0300

    made the navigation and header title transparent

--- app/pet-details/index.jsx
+++ app/pet-details/index.jsx
@@ -1,11 +1,23 @@
 
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useLocalSearchParams, useNavigation } from 'expo-router'
+import { useEffect } from 'react';
 
 export default function petDetails() {
+    const pet=useLocalSearchParams();
+    const navigation=useNavigation();
+
+    useEffect(()=>{
+                   
+        navigation.setOptions({
+            headerTransparent:true,
+            headerTitle:''    
+        })
+    },[])
   return (
     <View>
-      <Text>index</Text>
+      <Text>{JSON.stringify(pet)}</Text>
     </View>
   )
 }
