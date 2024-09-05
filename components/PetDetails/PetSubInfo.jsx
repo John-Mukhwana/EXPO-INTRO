2c6943ae496277398f8e1f1e3c8fcddfafaff883
John Mukhwana <Johnbradill67@gmail.com>
2024-09-05 18:53:22 +0300

    added PetSubInfo inside the index.jsx file inside petdetails folder

--- components/PetDetails/PetSubInfo.jsx
+++ components/PetDetails/PetSubInfo.jsx
@@ -1,11 +1,22 @@
 
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import Colors from '../../constants/Colors'
 
-export default function PetSubInfo() {
+export default function PetSubInfo({pet}) {
   return (
-    <View>
-      <Text>PetSubInfo</Text>
+    <View style={{padding:20}}>
+      <View>
+
+        <View style={{display:'flex', flexDirection:'row',alignItems:'center', backgroundColor:Colors.WHITE,padding:10,margin:10,borderRadius:8}}>
+            <Image source={require('./../../assets/images/calendar.png')} style={{width:40,height:40}}/>
+            <View>
+                <Text style={{fontFamily:'outfit',fontSize:16,color:Colors.GRAY}}>Age</Text>
+                <Text>{pet?.age}</Text>
+            </View>
+        </View>
+
+      </View>
     </View>
   )
 }
