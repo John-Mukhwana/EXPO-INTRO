fb7307c6441e8725e58dbf2fdb4297f9059a74fe
John Mukhwana <Johnbradill67@gmail.com>
2024-09-09 16:31:34 +0300

    added style to the copied view

--- components/PetDetails/PetSubInfo.jsx
+++ components/PetDetails/PetSubInfo.jsx
@@ -8,14 +8,14 @@ export default function PetSubInfo({pet}) {
     <View style={{padding:20}}>
       <View style={{display:'flex',flexDirection:'row'}} >
 
-        <View style={{display:'flex', flexDirection:'row',alignItems:'center', backgroundColor:Colors.WHITE,padding:10,margin:10,borderRadius:8}}>
+        <View style={{display:'flex',flex:1, flexDirection:'row',alignItems:'center', backgroundColor:Colors.WHITE,padding:10,margin:10,borderRadius:8}}>
             <Image source={require('./../../assets/images/calendar.png')} style={{width:40,height:40}}/>
             <View>
                 <Text style={{fontFamily:'outfit',fontSize:16,color:Colors.GRAY}}>Age</Text>
                 <Text>{pet?.age}</Text>
             </View>
         </View>
-        <View style={{display:'flex', flexDirection:'row',alignItems:'center', backgroundColor:Colors.WHITE,padding:10,margin:10,borderRadius:8}}>
+        <View style={{display:'flex',flex:1, flexDirection:'row',alignItems:'center', backgroundColor:Colors.WHITE,padding:10,margin:10,borderRadius:8}}>
             <Image source={require('./../../assets/images/calendar.png')} style={{width:40,height:40}}/>
             <View>
                 <Text style={{fontFamily:'outfit',fontSize:16,color:Colors.GRAY}}>Age</Text>
