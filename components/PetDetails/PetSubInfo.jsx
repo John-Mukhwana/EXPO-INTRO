bc60793245c50011ca74af8046ec8b2598e86418
John Mukhwana <Johnbradill67@gmail.com>
2024-09-09 16:26:14 +0300

    added the view twice

--- components/PetDetails/PetSubInfo.jsx
+++ components/PetDetails/PetSubInfo.jsx
@@ -6,8 +6,15 @@ import Colors from '../../constants/Colors'
 export default function PetSubInfo({pet}) {
   return (
     <View style={{padding:20}}>
-      <View>
+      <View >
 
+        <View style={{display:'flex', flexDirection:'row',alignItems:'center', backgroundColor:Colors.WHITE,padding:10,margin:10,borderRadius:8}}>
+            <Image source={require('./../../assets/images/calendar.png')} style={{width:40,height:40}}/>
+            <View>
+                <Text style={{fontFamily:'outfit',fontSize:16,color:Colors.GRAY}}>Age</Text>
+                <Text>{pet?.age}</Text>
+            </View>
+        </View>
         <View style={{display:'flex', flexDirection:'row',alignItems:'center', backgroundColor:Colors.WHITE,padding:10,margin:10,borderRadius:8}}>
             <Image source={require('./../../assets/images/calendar.png')} style={{width:40,height:40}}/>
             <View>
