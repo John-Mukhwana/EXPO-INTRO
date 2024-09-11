a1bf5490dfc6868a5e98fe617192ce2a70f45802
John Mukhwana <Johnbradill67@gmail.com>
2024-09-11 16:43:10 +0300

    turned the view to pressable Icon

---
+++ components/MarkFav.jsx
@@ -0,0 +1,11 @@
+
+import { View, Text, Pressable } from 'react-native'
+import React from 'react'
+
+export default function MarkFav() {
+  return (
+    <Pressable>
+     
+    </Pressable>
+  )
+}
