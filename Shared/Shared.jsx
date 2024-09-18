f3fec178d96fd0a2af6d87d66a7af88e3e65d921
John Mukhwana <Johnbradill67@gmail.com>
2024-09-18 13:51:15 +0300

    updated the shared Code

--- Shared/Shared.jsx
+++ Shared/Shared.jsx
@@ -3,21 +3,21 @@ import {doc,getDoc,setDoc, updateDoc} from 'firebase/firestore';
 import {db} from './../config/FirebaseConfig'
 
  const GetFavList=async(user)=>{
-    const docSnap=await getDoc(doc(db,'UserFavPet',user?.primaryEmailAddress?.emaiAddress))
+    const docSnap=await getDoc(doc(db,'UserFavPet',user?.primaryEmailAddress?.emailAddress))
     if(docSnap?.exists())
     {
         return docSnap.data();
     }
     else{
-        await setDoc(doc(db,'UserFavPet',user?.primaryEmailAddress?.emaiAddress),{
-            email:user?.primaryEmailAddress?.emaiAddress,
+        await setDoc(doc(db,'UserFavPet',user?.primaryEmailAddress?.emailAddress),{
+            email:user?.primaryEmailAddress?.emailAddress,
             favorites:[] 
         })
     }
 }
 
 const UpdateFav=async(user,favorites)=>{
-      const docRef=doc(db,'UserFavPet',user?.primaryEmailAddress?.emaiAddress)
+      const docRef=doc(db,'UserFavPet',user?.primaryEmailAddress?.emailAddress)
       try{
         await updateDoc(docRef,{
             favorites:favorites
