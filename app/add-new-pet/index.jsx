4d9c46e47e3f5cdb4ffa100be2ae947b8df12fa7
John Mukhwana <Johnbradill67@gmail.com>
2024-09-23 17:56:26 +0300

    Created the inputfileds and a submit button

--- app/add-new-pet/index.jsx
+++ app/add-new-pet/index.jsx
@@ -1,10 +1,12 @@
 
 
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image ,TextInput,StyleSheet} from 'react-native'
 import { useNavigation } from 'expo-router';
 import React, { useEffect } from 'react'
 import Colors from './../../constants/Colors';
-import { TextInput } from 'react-native-web';
+import { ScrollView } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+
 
 export default function AddNewPet() {
   const  navigation = useNavigation();
@@ -14,18 +16,95 @@ export default function AddNewPet() {
       title:'Add New Pet'
     })
   })
+  const handleInputChange=(fieldName,FieldValue)=>{
+    console.log(fieldName,FieldValue);
+  }
+
   return (
-    <View style={{padding:20}}>
+    <ScrollView style={{padding:20, }}>
       <Text style={{
         fontFamily:'outfit-medium',
         fontSize:16,
         }}>Add New Pet for adoption</Text>
-        <Image source={require('../../assets/images/placeholder.png')} style={{width:100, height: 100, marginTop:20,borderRadius:15 ,borderWidth:1,borderColor:Colors.GRAY}} />
+        <Image source={require('../../assets/images/placeholder.png')} style={{width:100, height: 100, borderRadius:15 ,borderWidth:1,borderColor:Colors.GRAY}} />
 
-        <View>
-          <Text>Pet Name*</Text>
-          <TextInput/>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Pet Name*</Text>
+          <TextInput style={styles.input}
+          onChangeText={(value=>handleInputChange('name',value))}
+          />
         </View>
-    </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Breed*</Text>
+          <TextInput style={styles.input}
+          onChangeText={(value=>handleInputChange('breed',value))}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Age*</Text>
+          <TextInput style={styles.input}
+          onChangeText={(value=>handleInputChange('age',value))}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Weight*</Text>
+          <TextInput style={styles.input}
+          onChangeText={(value=>handleInputChange('weight',value))}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Address*</Text>
+          <TextInput style={styles.input}
+          onChangeText={(value=>handleInputChange('addres',value))}
+          />
+        </View>
+
+        <View style={styles.inputContainer} >
+          <Text style={styles.label}>About*</Text>
+          <TextInput style={styles.input}
+          numberOfLines={5}
+          multiline={true}
+          onChangeText={(value=>handleInputChange('about',value))}
+          />
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={{fontFamily:'outfit-medium',textAlign:'center'}}>Submit</Text>
+        </TouchableOpacity>
+
+
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  inputContainer:{
+    marginVertical:5
+
+  },
+  input:{
+    padding:10,
+    backgroundColor:Colors.WHITE,
+    borderRadius:7,
+    borderWidth:1,
+    fontFamily:'outfit',
+    
+
+
+  },
+  label:{
+    marginVertical:5,
+    fontFamily:'outfit'
+
+  },
+  button:{
+    padding:15,
+    backgroundColor:Colors.PRIMARY,
+    borderRadius:7,
+    marginVertical:10
+  }
+  
+})
