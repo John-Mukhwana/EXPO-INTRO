57dbbe5208997a336e3b843052a850faffef4264
John Mukhwana <Johnbradill67@gmail.com>
2024-10-01 15:41:17 +0300

    created a ChatScreen

---
+++ app/Chat/index.jsx
@@ -0,0 +1,12 @@
+
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ChatScreen() {
+  return (
+    <View>
+      <Text>ChatScreen</Text>
+    </View>
+  )
+}
