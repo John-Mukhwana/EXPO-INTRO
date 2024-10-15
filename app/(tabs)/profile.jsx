fd59921b43d108fb85a6cfd6389828d277fd7302
John Mukhwana <Johnbradill67@gmail.com>
2024-10-15 08:01:52 +0300

    Added user Profile with routing

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,12 +1,97 @@
 
 
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, TouchableOpacity } from 'react-native'
 import React from 'react'
+import Ionicons from 'expo-vector-icons/Ionicons'
+import { useAuth, useUser } from '@clerk/clerk-expo'
+import Colors from '../constants/Colors'
+import { useRouter } from 'expo-router'
 
 export default function Profile() {
+  icon=Ionicons
+  const router=useRouter()
+  const {user}=useUser()
+  const {signOut}=useAuth()
+  const Menu=[
+    {
+      id:1,
+      name:'Add New Pet',
+      icon:'add-circle',
+      path:'/add-new-pe'
+    },
+    {
+      id:2,
+      name:'Favorites',
+      icon:'heart',
+      path:'/(tabs)/favorites'
+    },
+    {
+      id:3,
+      name:'Inbox',
+      icon:'chatbubble',
+      path:'/(tabs)/inbox'
+    },
+    {
+      id:4,
+      name:'exit',
+      path:'logout'
+    }
+  ]
+  const onPressMenu=(item)=>{
+    if(item.path=='logout'){
+      signOut();
+      return;
+    }
+    router.push(item.path)
+  }
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={{padding:20,margin:20}}>
+
+      <Text
+         style={{
+          fontFamily:'outfit-medium',
+          fontSize:20,
+         }}
+      >Profile
+      </Text>
+
+      <View style={{display:'flex',alignItems:'center',marginVertical:25}}>
+        <Image source={{uri:user?.imageUrl}} style={{width:80,height:80,borderRadius:99}} />
+        <Text style={{fontFamily:'outfit-medium',fontSize:20,marginTop:6}}>{user?.fullName}</Text>
+        <Text style={{fontFamily:'outfit-regular',fontSize:15,color:Colors.GRAY}}>{user?.primaryEmailAddress.emailAddress}</Text>
+      </View>
+      <FlatList 
+          data={Menu}
+          renderItem={({item,index})=>(
+           <TouchableOpacity
+            onPress={()=>onPressMenu(item)}
+            key={item.id}
+            style={{
+              display:'flex',
+              flexDirection:'row',
+              alignItems:'center',
+              justifyContent:'space-between',
+              marginVertical:10,
+              backgroundColor:Colors.LIGHT_GRAY,
+              padding:10,
+              borderRadius:10,
+           }}>
+            <Ionicons name={item.icon} size={30} color={Colors.PRIMARY}
+             style={{
+                 padding:10,
+                 backgroundColor:Colors.LIGHT_PRIMARY,
+                 borderRadius:10,
+               }}
+             />
+         </TouchableOpacity>
+          )}
+          />
+         <Text style={{
+            fontFamily:'outfit-medium',
+            fontSize:20,
+           }}>
+            {item.name}
+         </Text>
     </View>
   )
 }
